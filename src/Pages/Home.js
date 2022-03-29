d3d564c7c6e828c48f204702d879b0c5ad20fc81
Afsar Uddin <afsarwebdev@gmail.com>
2022-03-29 08:33:52 +0600

    product item

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import Products from '../components/HomeComps/Products';
 
 const Home = () => {
     window.scrollTo(0, 0);
     return (
         <div>
             <Header />
+            <Products />
         </div>
     );
 };
