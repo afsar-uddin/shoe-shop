53db99fe9e144c7b3e11bfcc3878861d8d71ee52
Afsar Uddin <afsarwebdev@gmail.com>
2022-03-29 08:41:26 +0600

    newsletter section

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import Products from '../components/HomeComps/Products';
+import NewsLetter from '../components/HomeComps/NewsLetter';
 
 const Home = () => {
     window.scrollTo(0, 0);
@@ -8,6 +9,7 @@ const Home = () => {
         <div>
             <Header />
             <Products />
+            <NewsLetter />
         </div>
     );
 };
